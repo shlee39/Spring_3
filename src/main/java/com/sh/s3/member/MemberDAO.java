b76e01b9410bbcd28fa1839c86bf1c20ec234eaf
shlee39 <39shlee@gmail.com>
2021-03-23 15:58:23 +0900

    Update MemberDAO.java

--- src/main/java/com/sh/s3/member/MemberDAO.java
+++ src/main/java/com/sh/s3/member/MemberDAO.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,17 +15,29 @@ import com.sh.s3.bankbook.BankBookDTO;
 
 @Repository
 public class MemberDAO {
+	@Autowired
+	private SqlSession sqlSession;
+	private final String NAMESPACE="com.sh.s3.member.memberDAO"; 
 	
 	public int memberUpdate(MemberDTO memberDTO) throws Exception{
 		//id를 제외하고 나머지를 수정
-		return sqlSession.update(NAMESPACE+".setUpdate", memberDTO);
-	
+		return sqlSession.update(NAMESPACE+".memberUpdate", memberDTO);
 	}
 	
 	public int memberDelete(MemberDTO memberDTO) throws Exception{
-		
+		return sqlSession.delete(NAMESPACE+".memberDelete", memberDTO);
 		
 	}
-
 	
+	public int memberJoin (MemberDTO memberDTO) throws Exception {
+	    int result = sqlSession.insert(NAMESPACE+".memberJoin", memberDTO);
+		return result;
+	}
+	
+
+	public MemberDTO memberLogin(MemberDTO memberDTO) throws Exception {
+		memberDTO = sqlSession.select(NAMESPACE+".memberLogin", memberDTO);	
+		return memberDTO;
+	}
 }
+

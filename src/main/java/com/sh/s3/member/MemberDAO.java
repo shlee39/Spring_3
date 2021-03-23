ccafe160d88b1c8336357410f76c6fcdebdedb47
shlee39 <39shlee@gmail.com>
2021-03-23 16:52:44 +0900

    1

--- src/main/java/com/sh/s3/member/MemberDAO.java
+++ src/main/java/com/sh/s3/member/MemberDAO.java
@@ -1,12 +1,5 @@
 package com.sh.s3.member;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;

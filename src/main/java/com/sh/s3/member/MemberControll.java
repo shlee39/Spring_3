854a9f29e36386ee48b8a35b53cd313bde97d71e
shlee39 <39shlee@gmail.com>
2021-03-24 15:41:54 +0900

    member

--- src/main/java/com/sh/s3/member/MemberControll.java
+++ src/main/java/com/sh/s3/member/MemberControll.java
@@ -1,5 +1,20 @@
 package com.sh.s3.member;
 
-public class MemberControll {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
+public class MemberControll {
+	
+	@Autowired
+	private MemberService memberService;
+	
+	
+	public void memberJoin(MemberDTO memberDTO)throws Exception {
+		
+	}
+	
+	public void memberLogin(MemberDTO memberDTO)throws Exception{
+		
+	}
 }
--- src/main/java/com/sh/s3/member/MemberService.java
+++ src/main/java/com/sh/s3/member/MemberService.java
@@ -1,5 +1,20 @@
 package com.sh.s3.member;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class MemberService {
 
+	private MemberDAO memberDAO;
+	
+	public int memberJoin(MemberDTO memberDTO)throws Exception{
+		return memberDAO.memberJoin(memberDTO);
+		
+	}
+	
+	public MemberDTO memberLogin(MemberDTO memberDTO)throws Exception{
+		return memberDAO.memberLogin(memberDTO);
+		
+	}
+	
 }

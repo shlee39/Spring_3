a771786ba175e6f50eb6d8718b75ee3d06291b75
shlee39 <39shlee@gmail.com>
2021-03-25 17:35:36 +0900

    account insert

---
+++ src/main/java/com/sh/s3/account/AccountDAO.java
@@ -0,0 +1,26 @@
+package com.sh.s3.account;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AccountDAO {
+
+	@Autowired
+	private SqlSession sqlSession;
+	private final String NAMESPACE="com.sh.s3.account.AccountDAO.";
+	
+	public List<AccountDTO> getList(AccountDTO accountDTO) throws Exception{
+		return sqlSession.selectList(NAMESPACE+"getList", accountDTO);
+	
+	}
+	
+	public int setInsert(AccountDTO accountDTO)throws Exception{
+		return sqlSession.delete(NAMESPACE+"setInsert", accountDTO);
+	}
+	
+	
+}

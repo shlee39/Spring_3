6f8136e8ba94c7b27d323b441fd135e6e6462366
shlee39 <39shlee@gmail.com>
2021-03-25 15:51:00 +0900

    account jsp

--- src/main/java/com/sh/s3/account/AccountController.java
+++ src/main/java/com/sh/s3/account/AccountController.java
@@ -1,5 +1,18 @@
 package com.sh.s3.account;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/account/**")
 public class AccountController {
+	
+	
+	@RequestMapping("accountList")
+	public void getList() throws Exception{
+		
+		
+	}
+	
 
 }
